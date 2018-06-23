6ee638ad62a948e541b82cb9880215460009cece
Thomas Ledoux <tledouxfr@gmail.com>
2018-06-23 12:47:11 +0200

    Allow upload of METS from ARK

--- SPARMETSViewer/views.py
+++ SPARMETSViewer/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """Definition of the routes for the application."""
 import os
+import sys
 
 from flask import request, render_template
 from flask_babel import gettext
+from requests import get  # to make GET request
 from werkzeug.utils import secure_filename
 
 from SPARMETSViewer import app, babel, db
@@ -19,8 +21,26 @@ def get_locale():
     return request.accept_languages.best_match(LANGUAGES.keys())
 
 
+def download(url, file_name):
+    """Download a url in the given file"""
+    # open in binary mode
+    with open(file_name, "wb") as file:
+        # get request
+        response = get(url)
+        # write to file
+        file.write(response.content)
+
+
+def from_ark_to_name(ark):
+    """Transform an ark in a name"""
+    name = ark.replace(":", "-").replace("/", "-").replace("--", "-")
+    if name.endswith(".xml"):
+        return name
+    return name + ".xml"
+
+
 def allowed_file(filename):
-    """ Return the files with allowed extensions"""
+    """Return the files with allowed extensions"""
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
@@ -36,7 +56,10 @@ def index():
 @app.route("/upload", methods=['GET', 'POST'])
 def render_page():
     """Access to the upload choice"""
-    return render_template('upload.html')
+    return render_template(
+        'upload.html',
+        ark_prefix=app.config['ARK_PREFIX'],
+        access_platform=app.config['ACCESS_PLATFORM'])
 
 
 @app.route('/uploadsuccess', methods=['GET', 'POST'])
@@ -48,13 +71,21 @@ def upload_file():
         # check if the post request has the file part
         if 'file' not in request.files:
             error = gettext('No file part')
-            return render_template('upload.html', error=error)
+            return render_template(
+                'upload.html',
+                error=error,
+                ark_prefix=app.config['ARK_PREFIX'],
+                access_platform=app.config['ACCESS_PLATFORM'])
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
             error = gettext('No selected file')
-            return render_template('upload.html', error=error)
+            return render_template(
+                'upload.html',
+                error=error,
+                ark_prefix=app.config['ARK_PREFIX'],
+                access_platform=app.config['ACCESS_PLATFORM'])
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             if not os.path.exists(app.config['UPLOAD_FOLDER']):
@@ -68,14 +99,67 @@ def upload_file():
             os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             if not success:
                 error = gettext('METS already exists')
-                return render_template('upload.html', error=error)
+                return render_template(
+                    'upload.html',
+                    error=error,
+                    ark_prefix=app.config['ARK_PREFIX'],
+                    access_platform=app.config['ACCESS_PLATFORM'])
             # Success back to index
             mets_instances = METS.query.all()
             success = gettext('Success! METS file uploaded!')
             return render_template('index.html', mets_instances=mets_instances, success=success)
         else:
             error = gettext('Not allowed selected file')
-            return render_template('upload.html', error=error)
+            return render_template(
+                'upload.html',
+                error=error,
+                ark_prefix=app.config['ARK_PREFIX'],
+                access_platform=app.config['ACCESS_PLATFORM'])
+
+
+@app.route('/retrievesuccess', methods=['GET', 'POST'])
+def retrieve_ark():
+    """Make the retrieval from ark"""
+    error = None
+    if request.method == 'POST':
+        ark = request.form.get("ark")
+        # check if the post request has the file part
+        if ark is None or not ark.startswith("b"):
+            error = gettext('No ARK part')
+            return render_template(
+                'upload.html',
+                error=error,
+                ark_prefix=app.config['ARK_PREFIX'],
+                access_platform=app.config['ACCESS_PLATFORM'])
+        access_platform = app.config['ACCESS_PLATFORM']
+        access_server = app.config['ACCESS_URL']
+        ark = app.config['ARK_PREFIX'] + ark
+        if access_platform == 'TEST':
+            url = access_server
+        else:
+            url = '%s/access/referenceDocumentRepository/%s.manifest' % (access_server, ark)
+        filename = from_ark_to_name(ark)
+
+        if not os.path.exists(app.config['UPLOAD_FOLDER']):
+            os.makedirs(app.config['UPLOAD_FOLDER'])
+        mets_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        download(url, mets_path)
+        aip_name = os.path.basename(filename)
+        mets = METSFile(mets_path, aip_name, access_platform)
+        success = mets.parse_mets()
+        # delete file from uploads folder
+        os.remove(mets_path)
+        if not success:
+            error = gettext('METS already exists')
+            return render_template(
+                'upload.html',
+                error=error,
+                ark_prefix=app.config['ARK_PREFIX'],
+                access_platform=app.config['ACCESS_PLATFORM'])
+        # Success back to index
+        mets_instances = METS.query.all()
+        success = gettext('Success! METS file uploaded!')
+        return render_template('index.html', mets_instances=mets_instances, success=success)
 
 
 # See https://www.ntu.edu.sg/home/ehchua/programming/webprogramming/Python3_Flask.html
--- config.py
+++ config.py
@@ -7,6 +7,11 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 UPLOAD_FOLDER = 'uploads'
+# None (to forbid Ark download), TEST (to download the sample METS), PFO, PFV, ...
+ACCESS_PLATFORM = 'TEST'
+# If TEST, the url of the sample METS, otherwise the url of the ACCESS module
+ACCESS_URL = 'http://localhost:5000/static/bpt6k6566369z.manifest.version0.release0.xml'
+ARK_PREFIX = 'ark:/12148/'
 ALLOWED_EXTENSIONS = set(['xml'])
 # available languages
 LANGUAGES = {
